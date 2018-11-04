73dabf9c7e67fc4aa0f5aca7ef6d2ba23bb77ffc
Andrzej Szczurek <andrzej.szczurek91@gmail.com>
2018-11-04 23:12:23 +0100

    Add project files.

---
+++ Mes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mes
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
